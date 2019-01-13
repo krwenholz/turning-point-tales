68b4608706a9d17f4cc415d1f752a6fd6d812255
Kyle R Wenholz <kyle@krwenholz.com>
2019-01-13 09:46:08 -0800

    Add logging

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,16 @@ extern crate lambda_runtime as lambda;
 extern crate log;
 #[macro_use]
 extern crate serde_derive;
-extern crate simple_logger;
+#[macro_use(slog_o)]
+extern crate slog;
+extern crate slog_async;
+extern crate slog_json;
+extern crate slog_scope;
+extern crate slog_stdlog;
 
 use lambda::error::HandlerError;
-
+use slog::Drain;
+use std::env;
 use std::error::Error;
 
 #[derive(Deserialize, Clone)]
@@ -22,7 +28,25 @@ struct CustomOutput {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    simple_logger::init_with_level(log::Level::Info)?;
+    let is_development = env::var("ENVIRONMENT")
+        .ok()
+        .unwrap_or("PRODUCTION".to_owned())
+        == "DEVELOPMENT";
+    let drain = slog_json::Json::new(std::io::stderr())
+        .set_pretty(is_development)
+        .add_default_keys()
+        .add_key_value(slog_o!("version" => env!("CARGO_PKG_VERSION")))
+        .build()
+        .fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+    let logger = slog::Logger::root(drain, slog_o!());
+    let _scope_guard = slog_scope::set_global_logger(logger);
+    let _log_guard = slog_stdlog::init().unwrap();
+
+    // Note: this `info!(...)` macro comes from `log` crate
+    info!("I'm in main!");
+    error!("I'm in main with error!");
+
     lambda!(my_handler);
 
     Ok(())
