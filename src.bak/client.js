8b9b9fd9a872a8f8b37a5c1fe3489f61b0d3c616
Kyle R Wenholz <kyle@krwenholz.com>
2019-05-08 16:31:30 -0700

    Cleaning out updated files

--- src.bak/client.js
+++
@@ -1,9 +0,0 @@
-import * as sapper from '../__sapper__/client.js';
-import {
-  Store
-} from 'svelte/store.js';
-
-sapper.start({
-  target: document.querySelector('#sapper'),
-  store: (data) => new Store(data),
-});
