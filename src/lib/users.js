bd47975f1916a79604fd548d8f55ed6881903f1b
Kyle R Wenholz <kyle@krwenholz.com>
2019-06-02 09:39:56 -0700

    Add TODOs and links for auth

--- src/lib/users.js
+++ src/lib/users.js
@@ -1,14 +1,12 @@
 import AWS from 'aws-sdk';
 import config from 'config';
 
-const poolData = {};
-if (config.get('environment') === 'production') {
-  poolData.UserPoolId = config.get('production.pool');
-  poolData.ClientId = config.get('production.client');
-} else {
-  poolData.UserPoolId = config.get('default.pool');
-  poolData.ClientId = config.get('default.client');
-}
-
-const userPool = AWS.AmazonCognitoIdentity.CognitoUserPool(poolData);
-
+// TODO(kyle): Add login and out with in-memory DB https://blog.risingstack.com/node-hero-node-js-authentication-passport-js/
+// TODO(kyle): Add sign up page with in-memory DB
+// TODO(kyle): Add real DB against Postgres, Probably move to express-session https://github.com/expressjs/session
+// TODO(kyle): Secure password storage https://github.com/emilbayes/secure-password
+// TODO(kyle): CSP https://sapper.svelte.dev/docs#Content_Security_Policy_CSP
+// TODO(kyle): Password reset flow
+// TODO(kyle): Triple check security https://hackernoon.com/your-node-js-authentication-tutorial-is-wrong-f1a3bf831a46
+// TODO(kyle): User edit page! (Get KC's help)
+// TODO(kyle): Rate limit shit https://github.com/nfriedly/express-rate-limit https://github.com/AdamPflug/express-brute
