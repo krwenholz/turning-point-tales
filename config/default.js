1507d88adf929eb4d6207bfd51f9024a2df10f97
Kyle R Wenholz <kyle@krwenholz.com>
2019-10-06 08:01:03 -0700

    Add hold on CircleCI

--- config/default.js
+++ config/default.js
@@ -1,6 +1,6 @@
 const dev = process.env.NODE_ENV === 'development';
 const customLogConfig = 'H2WIB_REQUEST_LOG: :remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"';
-const includeSSL = !(process.env['HOST'].match(/^localhost$/) || dev);
+const includeSSL = !((process.env.HOST || '').match(/^localhost$/) || dev);
 
 module.exports = {
   dev: dev,
@@ -9,17 +9,17 @@ module.exports = {
     format: dev ? 'dev' : customLogConfig,
   },
   sendGrid: {
-    apiKey: process.env['SEND_GRID_API_KEY'],
+    apiKey: process.env.SEND_GRID_API_KEY,
   },
   server: {
-    domain: process.env['HOST'],
+    domain: process.env.HOST,
     port: process.env.PORT,
     database: {
       connectionString: process.env.DATABASE_URL + (includeSSL ? '?ssl=true' : ''),
     }
   },
   stripe: {
-    secretKey: Buffer.from(process.env.STRIPE_SECRET_KEY).toString('base64'),
+    secretKey: Buffer.from(process.env.STRIPE_SECRET_KEY || '').toString('base64'),
     publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
     subscriptionId: process.env.STRIPE_SUBSCRIPTION_PLAN_ID,
   },
