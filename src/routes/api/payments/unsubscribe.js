0167bf13e2309a664ff63ced11e16558860a81cf
Kyle R Wenholz <kyle@krwenholz.com>
2019-10-06 06:59:54 -0700

    Minor payments cleanup

--- src/routes/api/payments/unsubscribe.js
+++ src/routes/api/payments/unsubscribe.js
@@ -1,7 +1,7 @@
 import Logger from 'js-logger';
 import config from 'config';
 import rp from 'request-promise-native';
-import { removeSubscriptionDetails } from 'src/lib/server/users';
+import { setSubscriptionDetails } from 'src/lib/server/users';
 
 /**
  * Cancels a subscription with Stripe and deletes our record.
@@ -27,7 +27,7 @@ const post = (req, res) => {
     .then((resp) => {
       Logger.info('Stripe unsubscribe succeeded', req.user.id, req.user.subscriptionId);
 
-      return removeSubscriptionDetails(req.user.id);
+      return setSubscriptionDetails(req.user.id, resp.customer, null, null);
     }).then(() => {
       res.send(JSON.stringify({'status': 'success'})).end();
       return;
@@ -36,7 +36,7 @@ const post = (req, res) => {
       if(err.statusCode == 404) {
         Logger.info('Stripe subscription not found, call it a success', req.user.id, req.user.subscriptionId);
         res.send(JSON.stringify({'status': 'success'})).end();
-        return removeSubscriptionDetails(req.user.id);
+        return setSubscriptionDetails(req.user.id, resp.customer, null, null);
       }
 
       Logger.error('Error unsubscribing customer', req.user.id, req.user.stripeCustomerId, err);
