d641f48180da206bd5dbfed86776cb414a68a121
Kyle R Wenholz <kyle@krwenholz.com>
2019-10-13 14:45:13 -0700

    Redirect www to root domain

---
+++ src/lib/server/require_root.js
@@ -0,0 +1,22 @@
+import Logger from 'js-logger';
+import config from 'config';
+
+/*
+ * Redirect to root domain.
+ */
+const requireRoot = (req, res, next) => {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (req.headers['host'].startsWith('www')) {
+    const url = 'https://' + config.get('server.domain') + req.url;
+    res.writeHead(302, {
+      Location: url
+    });
+    return res.end();
+  }
+  next();
+}
+
+export {
+  requireRoot
+}
+
