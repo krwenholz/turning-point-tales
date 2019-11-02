bbad90199103c5fe2403ebfacf6dd3f22ea1db66
Kristopher Paulsen <kristopherpaulsen@gmail.com>
2019-11-02 15:04:37 -0600

    Better than ever

---
+++ src/components/Form/Toggle.svelte
@@ -0,0 +1,51 @@
+<script>
+  let checked = false;
+  import { createEventDispatcher } from 'svelte';
+  const dispatch = createEventDispatcher();
+</script>
+
+<style>
+  /** Switch
+   -------------------------------------*/
+
+  .switch input {
+    position: absolute;
+    opacity: 0;
+  }
+
+  .switch {
+    display: inline-block;
+    font-size: 20px; /* 1 */
+    height: 1em;
+    width: 2em;
+    background: gray;
+    border-radius: 1em;
+  }
+
+  .switch div {
+    height: 1em;
+    width: 1em;
+    border-radius: 1em;
+    background: #FFF;
+    box-shadow: 0 0.1em 0.3em rgba(0,0,0,0.3);
+    transition: all 300ms;
+  }
+
+  .switch input:checked + div {
+    transform: translate3d(100%, 0, 0);
+  }
+
+  .checked {
+    background: var(--root-call-to-action);
+  }
+</style>
+
+<label class='switch' class:checked>
+  <input
+    on:input
+    on:click
+    bind:checked={checked}
+    type="checkbox"
+  />
+  <div></div>
+</label>
