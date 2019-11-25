1ea2fadc1a435c285737555a9ab422d57d9ed71a
Kyle R Wenholz <kyle@krwenholz.com>
2019-11-24 21:54:37 -0800

    Standardize seed accounts

--- cypress/integration/password-reset.spec.js
+++ cypress/integration/password-reset.spec.js
@@ -1,47 +1,47 @@
-describe('Password Reset Flow: Submit an email', () => {
-  it('can visit the page without authentication', () => {
-    cy.visit('/password-reset')
-  })
+describe("Password Reset Flow: Submit an email", () => {
+  it("can visit the page without authentication", () => {
+    cy.visit("/password-reset");
+  });
 
-  it('displays errors and success on reset flow', () => {
+  it("displays errors and success on reset flow", () => {
     cy.logIn()
-      .visit('/user/login')
+      .visit("/user/login")
       .get('[data-cy="password-reset"]')
-      .invoke('attr', 'href')
+      .invoke("attr", "href")
       .then(cy.visit);
 
-    cy.get('input')
-      .type('jefh2wib.com')
-      .get('button')
+    cy.get("input")
+      .type("jefh2wib.com")
+      .get("button")
       .click()
-      .get('.error-msg')
-      .should('contain', 'Could not find')
+      .get(".error-msg")
+      .should("contain", "Could not find");
 
-    cy.get('input')
+    cy.get("input")
       .clear()
-      .type('jeff@h2wib.com')
-      .get('button')
-      .click()
+      .type("test-subscriber@h2wib.com")
+      .get("button")
+      .click();
 
-    cy.get('h3')
-      .should('contain', 'You have been sent an email')
+    cy.get("h3").should("contain", "You have been sent an email");
   });
 });
 
-describe('Password Reset Flow: Reset your password', () => {
-  const dummyToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+describe("Password Reset Flow: Reset your password", () => {
+  const dummyToken =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
 
-  it('can visit the page without authentication', () => {
-    cy.visit(`/password-reset?id=0&token=${dummyToken}`)
-  })
+  it("can visit the page without authentication", () => {
+    cy.visit(`/password-reset?id=0&token=${dummyToken}`);
+  });
 
-  it('displays error on expired or fake id/token data', () => {
+  it("displays error on expired or fake id/token data", () => {
     cy.visit(`/password-reset?id=0&token=${dummyToken}`)
-      .get('input')
-      .type('fake-password')
-      .get('button')
+      .get("input")
+      .type("fake-password")
+      .get("button")
       .click()
-      .get('.error-msg')
-      .should('contain', 'Unable to update your password')
+      .get(".error-msg")
+      .should("contain", "Unable to update your password");
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,13 +1,13 @@
-Cypress.Commands.add('logIn', () => {
-  cy.visit('/user/login');
-  cy.get('#email').type('jeff@h2wib.com');
-  cy.get('#password').type('foo');
-  cy.contains('Log in').click();
+Cypress.Commands.add("logIn", () => {
+  cy.visit("/user/login");
+  cy.get("#email").type("test-subscriber@h2wib.com");
+  cy.get("#password").type("foo");
+  cy.contains("Log in").click();
 });
 
-Cypress.Commands.add('logInNonSubscriber', () => {
-  cy.visit('/user/login');
-  cy.get('#email').type('test-nonsubscriber@h2wib.com');
-  cy.get('#password').type('foo');
-  cy.contains('Log in').click();
+Cypress.Commands.add("logInNonSubscriber", () => {
+  cy.visit("/user/login");
+  cy.get("#email").type("test-nonsubscriber@h2wib.com");
+  cy.get("#password").type("foo");
+  cy.contains("Log in").click();
 });
