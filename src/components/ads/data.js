87d9718c23d86bb6c156a386d5ea339cda4ab88f
Kyle R Wenholz <kyle@krwenholz.com>
2019-11-24 21:22:23 -0800

    Fix some ad and user type things

--- src/components/ads/data.js
+++ src/components/ads/data.js
@@ -1,239 +1,242 @@
 const choicesAndAttributes = {
-  "Reading recommendations from the founders and creators": ["founder-read", "creator-read"],
+  "Reading recommendations from the founders and creators": [
+    "founder-read",
+    "creator-read"
+  ],
   "Reading adjacent things": ["founder-read-help", "creator-read-help"],
-  "Reading orthogonal things": ["founder-random", "creator-random"],
+  "Reading orthogonal things": ["founder-random", "creator-random"]
 };
 
 const rawContent = [
   {
-    links: {
+    links: [
       {
-        source: 'Amazon',
-        url: "https://amazon.com/gp/product/B07TDQSCTW",
-      },
-    },
+        source: "Amazon",
+        url: "https://amazon.com/gp/product/B07TDQSCTW"
+      }
+    ],
     attributes: ["founder-read"],
     description: [
       "Step into another world with a trusted storyteller. The Nazis won, but there's a resistance brewing."
     ]
   },
   {
-    links: {
+    links: [
       {
-        source: 'Amazon',
-        url: "https://amazon.com/dp/0553496395",
-      },
-    },
+        source: "Amazon",
+        url: "https://amazon.com/dp/0553496395"
+      }
+    ],
     attributes: ["founder-read", "founder-random"],
     description: [
       "Read a new tale, one where you follow directions and make something tasty! The folks at Half Baked Harvest will help you gear up with a full tummy before your next tale."
     ]
   },
   {
-    links: {
+    links: [
       {
-        source: 'Amazon',
-        url: "https://amazon.com/dp/product/B07CXG6C9W",
-      },
-    },
+        source: "Amazon",
+        url: "https://amazon.com/dp/product/B07CXG6C9W"
+      }
+    ],
     attributes: ["founder-read-help"],
     description: [
       "Reading is fun. It's even more fun when the library and more are at your fingertips. Check out a Kindle for an easy screen reading experience."
     ]
   },
   {
-    links: {
+    links: [
       {
-        source: 'Amazon',
-        url: "https://amazon.com/dp/B000H1217M",
-      },
-    },
+        source: "Amazon",
+        url: "https://amazon.com/dp/B000H1217M"
+      }
+    ],
     attributes: ["founder-random"],
     description: [
       "One of us has a dog. That dog loves being smart. Why? Because smart puppies get Zuke's! Who's a good girl?!"
     ]
   },
   {
-    links: {
+    links: [
       {
-        source: 'Amazon',
-        url: "https://amazon.com/dp/B07NDNZ6XS",
-      },
-    },
+        source: "Amazon",
+        url: "https://amazon.com/dp/B07NDNZ6XS"
+      }
+    ],
     attributes: ["founder-random"],
     description: [
       "Arnold gets a bit crazy. Or maybe not. Either way, Total Recall is a gem. You should watch it."
     ]
   },
   {
-    links: {
+    links: [
       {
-        source: 'Amazon',
-        url: "https://amazon.com/dp/B00PGWI6QC",
-      },
-    },
+        source: "Amazon",
+        url: "https://amazon.com/dp/B00PGWI6QC"
+      }
+    ],
     attributes: ["founder-random"],
     description: [
       "One of Arnold's first appearances. He's strong. He's a businessman. And he may be unintentionally funny. Check out Andre the Giant and a few other strong men in this fabled documentary."
     ]
   },
   {
-    links: {
+    links: [
       {
-        source: 'Amazon',
-        url: "https://amazon.com/dp/B07FZ8S74R",
-      },
-    },
+        source: "Amazon",
+        url: "https://amazon.com/dp/B07FZ8S74R"
+      }
+    ],
     attributes: ["founder-read-help"],
     description: ["Alexa! Thanks for being so cool."]
   },
   {
-    links: {
+    links: [
       {
-        source: 'Amazon',
-        url: "https://amazon.com/dp/B06XCPGFP3",
-      },
-    },
+        source: "Amazon",
+        url: "https://amazon.com/dp/B06XCPGFP3"
+      }
+    ],
     attributes: ["founder-random"],
     description: [
       "You have a bike? We like bikes. Hopefully you like yours too. Don't keep that a secret, but do keep it safe. Lock it up!"
     ]
   },
   {
-    links: {
+    links: [
       {
-        source: 'Amazon',
-        url: "ihttps://amazon.com/dp/B000AJIF4E",
-      },
-    },
+        source: "Amazon",
+        url: "ihttps://amazon.com/dp/B000AJIF4E"
+      }
+    ],
     attributes: ["founder-random"],
     description: ["Best headphones. Ever."]
   },
   {
-    links: {
+    links: [
       {
-        source: 'Amazon',
-        url: "https://amazon.com/dp/B001GI94SO",
-      },
-    },
+        source: "Amazon",
+        url: "https://amazon.com/dp/B001GI94SO"
+      }
+    ],
     attributes: ["founder-random"],
     description: [
       "Contacts? Dry eyes? Check out this solution. Hah. See what we did there?"
     ]
   },
   {
-    links: {
+    links: [
       {
-        source: 'Amazon',
-        url: "https://amazon.com/gp/video/detail/B0021Y8RW6",
-      },
-    },
+        source: "Amazon",
+        url: "https://amazon.com/gp/video/detail/B0021Y8RW6"
+      }
+    ],
     attributes: ["founder-random"],
     description: [
       "Park your butt for soome good old recreation. Enjoy the comedic stylings in this fabled series. Their jokes are way better than ours."
     ]
   },
   {
-    links: {
+    links: [
       {
-        source: 'Amazon',
-        url: "https://www.indiebound.org/book/9780756404741",
-      },
-    },
+        source: "Amazon",
+        url: "https://www.indiebound.org/book/9780756404741"
+      }
+    ],
     attributes: ["founder-read"],
     description: [
       "Patrick Rothfuss is a masterful storyteller. Dig in to this tale and you won't be disappointed."
     ]
   },
   {
-    links: {
+    links: [
       {
-        source: 'Amazon',
-        url: "https://www.indiebound.org/book/9780553293357",
-      },
-    },
+        source: "Amazon",
+        url: "https://www.indiebound.org/book/9780553293357"
+      }
+    ],
     attributes: ["founder-read"],
     description: ['Consider this "foundational" reading in Science Fiction.']
   },
   {
-    links: {
+    links: [
       {
-        source: 'Amazon',
-        url: "https://www.indiebound.org/book/9780618640157",
-      },
-    },
+        source: "Amazon",
+        url: "https://www.indiebound.org/book/9780618640157"
+      }
+    ],
     attributes: ["founder-read"],
     description: [
       "We like fantasy. We think you might too. It's a long read and a little old, but Lord of the Rings is where a lot of modern fantasy tropes began, for very good reason."
     ]
   },
   {
-    links: {
+    links: [
       {
-        source: 'Amazon',
-        url: "https://www.indiebound.org/book/9780140067477",
-      },
-    },
+        source: "Amazon",
+        url: "https://www.indiebound.org/book/9780140067477"
+      }
+    ],
     attributes: ["founder-read"],
     description: [
       "Pooh Bear has many lessons to teach us. Mr. Hoff uses that in a most enjoyable way to introduce the teachings of Taoism. A worthwhile read, even if you aren't looking to be an uncarved block."
     ]
   },
   {
-    links: {
+    links: [
       {
-        source: 'Amazon',
-        url: "https://www.indiebound.org/book/9780345479211",
-      },
-    },
+        source: "Amazon",
+        url: "https://www.indiebound.org/book/9780345479211"
+      }
+    ],
     attributes: ["founder-read"],
     description: [
       "In need of a suspenseful science fiction masterpiece? Check out a Peter F. Hamilton treasure."
     ]
   },
   {
-    links: {
+    links: [
       {
-        source: 'Amazon',
-        url: "https://www.indiebound.org/book/9780312156961",
-      },
-    },
+        source: "Amazon",
+        url: "https://www.indiebound.org/book/9780312156961"
+      }
+    ],
     attributes: ["founder-read"],
     description: [
       "Do you like sweeping stories? A bit of grit? Bernard Cornwell introduces us to a more real Arthur than any other. One we can feel and weep for."
     ]
   },
   {
-    links: {
+    links: [
       {
-        source: 'Amazon',
-        url: "https://www.indiebound.org/book/9780142302378",
-      },
-    },
+        source: "Amazon",
+        url: "https://www.indiebound.org/book/9780142302378"
+      }
+    ],
     attributes: ["founder-read"],
     description: [
       "Cheer for the critters in this fantasy adventure. Only problem? Once you're hooked, there are over a dozen more stories in this universe to sink your fangs into."
     ]
   },
   {
-    links: {
+    links: [
       {
-        source: 'Amazon',
-        url: "https://www.indiebound.org/book/9780316204262",
-      },
-    },
+        source: "Amazon",
+        url: "https://www.indiebound.org/book/9780316204262"
+      }
+    ],
     attributes: ["founder-read"],
     description: [
       "Based in Seattle, Bernadette goes missing. It's a fun ride hunting her down."
     ]
   },
   {
-    links: {
+    links: [
       {
-        source: 'Amazon',
-        url: "https://www.indiebound.org/book/9780671027032",
-      },
-    },
+        source: "Amazon",
+        url: "https://www.indiebound.org/book/9780671027032"
+      }
+    ],
     attributes: ["founder-read"],
     description: [
       "Everyone needs friends. It's worth learning a bit about how to win them over. Sound and simple advice in a digestible form."
