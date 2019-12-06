456746614f70c5e1278c048e326430b40b54b17c
Kyle R Wenholz <kyle@krwenholz.com>
2019-12-05 20:57:52 -0800

    Make user creation use navigation

--- cypress/integration/non-subscriber.spec.js
+++ cypress/integration/non-subscriber.spec.js
@@ -1,18 +1,26 @@
-describe('NonSubscriber', () => {
+describe("NonSubscriber", () => {
   beforeEach(() => {
     cy.logInNonSubscriber();
   });
 
-  it('displays ads', () => {
-    cy.contains('Continue...').click()
-      .get('#ad-options button:first').click();
-    cy.contains('Now back to the regularly scheduled programming....').click()
-      .url().should('match', /\/story\/[a-z0-9-]+\?storyNode=start/);
+  it("displays ads", () => {
+    cy.contains("Continue...").click();
+    cy.contains(
+      /(Reading recommendations from the founders and creators|Reading adjacent things|Reading orthogonal things)/
+    ).click();
+    cy.contains("Now back to the regularly scheduled programming....")
+      .click()
+      .url()
+      .should("match", /\/story\/[a-z0-9-]+\?storyNode=start/);
   });
 
-  it('blocks early content', () => {
-    cy.contains('Subscribers only before ').should('exist');
-    cy.contains('Become a full adventurer now to unlock access.').should('exist');
-    cy.get('.story-previews').find('button').should('have.length', 1);
+  it("blocks early content", () => {
+    cy.contains("Subscribers only before ").should("exist");
+    cy.contains("Become a full adventurer now to unlock access.").should(
+      "exist"
+    );
+    cy.get(".story-previews")
+      .find("button")
+      .should("have.length", 1);
   });
 });
--- cypress/support/utils/index.js
+++ cypress/support/utils/index.js
@@ -1,59 +1,59 @@
-const uuidv4 = require('uuid/v4');
-const {
-  entries
-} = Object;
+const uuidv4 = require("uuid/v4");
+const { entries } = Object;
 
-export const createUser = (cy) => {
+export const createUser = cy => {
   const uuid = uuidv4();
 
   const user = {
     firstName: `Test${uuid}`,
     lastName: `McTestface${uuid}`,
     email: `${uuid}@h2wib.com`,
-    password: 'test',
-    confirmPassword: 'test',
+    password: "test",
+    confirmPassword: "test"
   };
 
-  cy.visit('/user/new')
+  // Get there like a normal user.
+  cy.visit("/")
+    .contains("Login")
+    .click();
+  cy.contains("create an account").click();
+  cy.url().should("match", /\/user\/new/);
 
   entries(user).forEach(([key, value]) => {
-    cy.get('#' + key)
-      .type(value);
+    cy.get("#" + key).type(value);
   });
 
-  cy.get('#tos_and_privacy')
+  cy.get("#tos_and_privacy")
     .click()
-    .get('button')
-    .contains('Sign up')
+    .get("button")
+    .contains("Sign up")
     .click();
 
   return user;
-}
+};
 
-export const deleteUser = (cy) => {
-  cy.visit('/user/profile')
-    .get('button')
-    .contains('Settings')
+export const deleteUser = cy => {
+  cy.visit("/user/profile")
+    .get("button")
+    .contains("Settings")
     .click()
-    .get('button')
-    .contains('DELETE')
+    .get("button")
+    .contains("DELETE")
     .click();
 };
 
-export const logOut = (cy) => {
-  cy.wait(100)
-    .visit('/api/user/logout')
-}
+export const logOut = cy => {
+  cy.wait(100).visit("/api/user/logout");
+};
 
-export const logInAs = (cy, {
-  email,
-  password
-}) => {
-  cy.request('/api/user/logout')
-    .visit('/user/login')
-    .get('#email').type(email)
-    .get('#password').type(password)
-    .get('button')
-    .contains('Log in')
-    .click()
+export const logInAs = (cy, { email, password }) => {
+  cy.request("/api/user/logout")
+    .visit("/user/login")
+    .get("#email")
+    .type(email)
+    .get("#password")
+    .type(password)
+    .get("button")
+    .contains("Log in")
+    .click();
 };
