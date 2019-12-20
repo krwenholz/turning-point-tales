5bc7a9b9e5fa8568342e6005c6aca141a8e32f5d
Kristopher Paulsen <kristopherpaulsen@gmail.com>
2019-12-20 16:00:02 -0700

    wrapping element for simpler resizing

--- src/routes/user/new.svelte
+++ src/routes/user/new.svelte
@@ -54,6 +54,11 @@
 </script>
 
 <style>
+  .user-new {
+    max-width: 720px;
+    margin: auto;
+  }
+
   p {
     text-align: left;
     margin-bottom: 0;
@@ -82,36 +87,36 @@
   <title>User Creation</title>
 </svelte:head>
 
-{#if $page.query.error === 'unknown'}
-  <section class="errors text">
+<section class='user-new'>
+  {#if $page.query.error === 'unknown'}
+    <article class="errors">
+      <p>
+        An unknown error occurred! Please try again or contact
+        <a href="mailto:support@turningpointtales.com">
+          support@turningpointtales.com
+        </a>
+        .
+      </p>
+    </article>
+  {/if}
+
+  <article>
+    <h1>Let's create your user!</h1>
+
     <p>
-      An unknown error occurred! Please try again or contact
-      <a href="mailto:support@turningpointtales.com">
-        support@turningpointtales.com
-      </a>
-      .
+      Creating an account grants you access to the entire library of adventures.
+      We use your email and name, which we only share for things like payments, to
+      personalize the experience and send you exciting updates. In lieu of sharing
+      your data and browsing with a third party, we handcraft some interesting ads
+      or let you skip those entirely with a paid subscription.
     </p>
-  </section>
-{/if}
 
-<section class="preamble text">
-  <h1>Let's create your user!</h1>
+    <p>
+      More questions? Check out our
+      <a href="/faq">FAQ.</a>
+    </p>
+  </article>
 
-  <p>
-    Creating an account grants you access to the entire library of adventures.
-    We use your email and name, which we only share for things like payments, to
-    personalize the experience and send you exciting updates. In lieu of sharing
-    your data and browsing with a third party, we handcraft some interesting ads
-    or let you skip those entirely with a paid subscription.
-  </p>
-
-  <p>
-    More questions? Check out our
-    <a href="/faq">FAQ.</a>
-  </p>
-</section>
-
-<section class="form text">
   <Form on:submit="{handleSubmit}" bind:this="{form}">
     {#if errorMsg}
       <span class="error form-group">{errorMsg}</span>
@@ -177,11 +182,11 @@
       Sign up
     </Button>
   </Form>
-</section>
 
-<section class="text">
-  <p>
-    If you already have an account with us, you can log in
-    <a href="/user/login">here.</a>
-  </p>
+  <article>
+    <p>
+      If you already have an account with us, you can log in
+      <a href="/user/login">here.</a>
+    </p>
+  </article>
 </section>
