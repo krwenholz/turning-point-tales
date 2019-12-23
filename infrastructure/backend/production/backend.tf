0466c349b7e8babc0521de1987529fbfa5d55c8d
Kyle R Wenholz <kyle@krwenholz.com>
2019-12-23 07:35:26 -0800

    Add basic ElasticSearch

---
+++ infrastructure/backend/production/backend.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+module "prod_backend" {
+  source     = "git::https://github.com/cloudposse/terraform-aws-tfstate-backend.git?ref=master"
+  namespace  = "h2wib"
+  stage      = "prod"
+  name       = "terraform"
+  attributes = ["state"]
+  region     = "us-west-2"
+}
--- infrastructure/project/project.tf
+++ infrastructure/project/project.tf
@@ -5,22 +5,16 @@ variable stage { type = "string" }
 # Resources
 #######################################################################
 module "logdrain" {
-  source               = "git::https://github.com/krwenholz/heroku_cloudwatch_sync.git?ref=master"
-  logger_name          = "our_first_big_thing-logdrain-${var.stage}"
-  region               = "us-west-2"
-  app_name             = "our_first_big_thing-${var.stage}"
+  source      = "git::https://github.com/krwenholz/heroku_cloudwatch_sync.git?ref=master"
+  logger_name = "our_first_big_thing-logdrain-${var.stage}"
+  region      = "us-west-2"
+  app_name    = "our_first_big_thing-${var.stage}"
 }
 
-module "log_processor" {
-  source               = "git::https://github.com/krwenholz/goaccess_on_aws.git?ref=master"
-  prefix               = "our_first_big_thing-${var.stage}"
-  region               = "us-west-2"
-  configurations       = [
-    {
-      log_group : "our_first_big_thing-${var.stage}",
-      bucket_name : "our-first-big-thing-${var.stage}-logs"
-    }
-  ]
+module "elasticsearch" {
+  source = "./elasticsearch"
+
+  stage = "staging"
 }
 
 # Outputs
@@ -28,4 +22,3 @@ module "log_processor" {
 output "url" {
   value = module.logdrain.url
 }
-
