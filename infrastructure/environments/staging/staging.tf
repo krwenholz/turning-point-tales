3e95c4d078df290d29a6a3fccebd72696cfda818
Kyle R Wenholz <kyle@krwenholz.com>
2019-12-23 07:35:26 -0800

    Add basic ElasticSearch

--- infrastructure/environments/staging/staging.tf
+++ infrastructure/environments/staging/staging.tf
@@ -7,9 +7,9 @@ terraform {
 
   backend "s3" {
     region         = "us-west-2"
-    bucket         = "h2wib-staging-terraform-state"
+    bucket         = "h2wib-development-terraform-state"
     key            = "our_first_big_thing.tfstate"
-    dynamodb_table = "h2wib-staging-terraform-state-lock"
+    dynamodb_table = "h2wib-development-terraform-state-lock"
     encrypt        = true
   }
 }
@@ -17,7 +17,7 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   version = "~> 2.0"
-  region = "us-west-2"
+  region  = "us-west-2"
 }
 
 module "project" {
