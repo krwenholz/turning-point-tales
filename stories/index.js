e0d44c87d784e0971b876e7de054261817b0b088
K.C <kristopherpaulsen@gmail.com>
2019-12-31 15:13:00 -0700

    Update seans forgotten forest squashed (#11)
    * basic story uploaded
    
    * add badges... yay!

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,5 @@
 const mr_banks = require("./mr_banks.js");
 const wagons_and_dragons = require("./wagons_and_dragons.js");
+const forgotten_forest = require('./forgotten_forest.js');
 
-module.exports = [mr_banks, wagons_and_dragons];
+module.exports = [mr_banks, wagons_and_dragons, forgotten_forest];
