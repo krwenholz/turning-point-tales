27fbb7149ed32f48e48f5fbca7170366ffb08b4b
Kristopher Paulsen <kristopherpaulsen@gmail.com>
2020-01-12 18:06:11 -0700

    cha cha changes

--- src/components/EditStory/_StoryText.svelte
+++ src/components/EditStory/_StoryText.svelte
@@ -1,5 +1,6 @@
 <script>
   import TextArea from 'src/components/TextArea';
+  import Minus from 'src/components/icons/Minus.svelte';
   import { createEventDispatcher, afterUpdate } from "svelte";
   import { get, toPath, join, cloneDeep } from 'lodash';
 
@@ -26,32 +27,51 @@
     flex-flow: column;
   }
 
+  p {
+    position: relative;
+    display: flex;
+    align-items: center;
+    margin: 0 0 16px 0;
+    padding: 0
+  }
+
+  .story-text :global(svg) {
+    position: absolute;
+    top: 0;
+    left: -1%;
+    width: 26px;
+  }
+
   .story-text :global(.text-area) {
-    margin-bottom: 16px;
+    flex: 1;
+    padding-left: 24px;
   }
 </style>
 
 <div class='story-text'>
   {#each text as paragraph, idx}
-    <TextArea
-      value={paragraph}
-      bind:this={nodes[[storyIdx, 'story', 'text', idx]]}
-      on:input={ e => onInput(e, {
+    <p>
+      <Minus />
+      <TextArea
+        value={paragraph}
+        bind:this={nodes[[storyIdx, 'story', 'text', idx]]}
+        on:input={ e => onInput(e, {
         idx,
         storyIdx,
         storyNode,
         typeOfChange: 'storyText',
         path: [storyIdx, 'story', 'text', idx],
         prevValue: paragraph,
+        })}
+        on:keydown={ e => onKeydown(e, {
+      idx,
+      storyIdx,
+      storyNode,
+      typeOfChange: 'storyText',
+      path: [storyIdx, 'story', 'text', idx],
+      prevValue: paragraph,
       })}
-      on:keydown={ e => onKeydown(e, {
-        idx,
-        storyIdx,
-        storyNode,
-        typeOfChange: 'storyText',
-        path: [storyIdx, 'story', 'text', idx],
-        prevValue: paragraph,
-      })}
-    />
+      />
+    </p>
   {/each}
 </div>
