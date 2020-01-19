1d717bbde936efff725ae6a3ca6c0b626db9d284
Kyle R Wenholz <kyle@krwenholz.com>
2020-01-19 07:36:07 -0800

    Make Alexa speak

---
+++ src/routes/api/alexa/intent_handlers/_help.js
@@ -0,0 +1,20 @@
+// TODO(kyle): this is really stupid and needs help
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return (
+      handlerInput.requestEnvelope.request.type === "IntentRequest" &&
+      handlerInput.requestEnvelope.request.intent.name === "AMAZON.HelpIntent"
+    );
+  },
+  handle(handlerInput) {
+    const speechText = "You can say hello to me!";
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .withSimpleCard("Hello World", speechText)
+      .getResponse();
+  }
+};
+
+export default HelpIntentHandler;
