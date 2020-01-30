484f0f5c48ff8848e7cbd928ca544b039f2ab006
Kyle R Wenholz <kyle@krwenholz.com>
2020-01-29 20:36:05 -0800

    Make Alexa list stories and get one

--- src/routes/api/alexa/intent_handlers/_list_stories.js
+++ src/routes/api/alexa/intent_handlers/_list_stories.js
@@ -3,47 +3,6 @@ import { pool } from "src/lib/server/database.js";
 import { map, join } from "lodash";
 import { logger } from "src/logging";
 
-const ListStoriesIntentHandler = {
-  canHandle(handlerInput) {
-    return (
-      handlerInput.requestEnvelope.request.type === "IntentRequest" &&
-      handlerInput.requestEnvelope.request.intent.name === "ListStories"
-    );
-  },
-  handle(handlerInput) {
-    return pool.query(listAllQuery).then(results => {
-      const storyTitleChoices = map(results.rows, story => {
-        return {
-          id: `${story.id}`,
-          name: {
-            value: story.title,
-            synonyms: []
-          }
-        };
-      });
-
-      const replaceEntityDirective = {
-        type: "Dialog.UpdateDynamicEntities",
-        updateBehavior: "REPLACE",
-        types: [
-          {
-            name: "STORY_TITLE_CHOICE",
-            values: storyTitleChoices
-          }
-        ]
-      };
-
-      const repeat = "Which tale is next?";
-      const speechText = "Let's get you adventuring. " + repeat;
-
-      return handlerInput.responseBuilder
-        .speak(speechText)
-        .reprompt(repeat)
-        .addDirective(replaceEntityDirective)
-        .withSimpleCard("Story choices", speechText)
-        .getResponse();
-    });
-  }
-};
+const ListStoriesIntentHandler = {};
 
 export default ListStoriesIntentHandler;
