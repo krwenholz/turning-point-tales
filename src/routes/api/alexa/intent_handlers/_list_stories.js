8d7e0ec1e21d88079d807120cd83590c019ab60d
Kyle R Wenholz <kyle@krwenholz.com>
2020-02-02 06:02:38 -0800

    List things with Alexa

--- src/routes/api/alexa/intent_handlers/_list_stories.js
+++ src/routes/api/alexa/intent_handlers/_list_stories.js
@@ -1,5 +1,3 @@
-import { listAllQuery } from "src/routes/story/_stories";
-import { pool } from "src/lib/server/database.js";
 import { map, join } from "lodash";
 import { logger } from "src/logging";
 
