c22eb0bbbb8916d8f502d01cbc97b1d8aa619693
Kristopher Paulsen <kristopherpaulsen@gmail.com>
2020-02-08 09:18:42 -0700

    wip

---
+++ src/routes/experimental/editor.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { editorBackup } from 'src/lib/global-state-stores/browserStore/editor-backup.js'
+  import Editor from 'src/components/Editor/index';
+
+  let story = editorBackup();
+</script>
+
+<svelte:head>
+  <title>Editor</title>
+</svelte:head>
+
+<Editor
+  story={$story}
+  onEdit={updatedStory => $story = updatedStory}
+/>
+
+<!--<section class="graph">-->
+<!--  {#if storyIsValid}-->
+<!--    <h2>Preview</h2>-->
+<!--    <Graph {story} />-->
+<!--  {:else}-->
+<!--    <p class='error'> current story is invalid </p>-->
+<!--  {/if}-->
+<!--</section>-->
