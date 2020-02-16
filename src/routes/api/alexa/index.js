b49ccadd1b54fa3c33a03a4afaee7e8b03f51be7
Kyle R Wenholz <kyle@krwenholz.com>
2020-02-15 16:31:55 -0800

    Clean up Alexa code

--- src/routes/api/alexa/index.js
+++ src/routes/api/alexa/index.js
@@ -1,312 +1,20 @@
 import * as Alexa from "ask-sdk-core";
 import CancelAndStopIntentHandler from "./intent_handlers/_cancel_and_stop";
+import DecisionGivenChooseStoryDecisionIntentHandler from "./intent_handlers/_decision_given_choose_story_decision";
 import ErrorHandler from "./_error_handler";
+import GoBackIntentHandler from "./intent_handlers/_go_back";
 import HelpIntentHandler from "./intent_handlers/_help";
 import LaunchRequestHandler from "./request_handlers/_launch";
 import ListStoriesIntentHandler from "./intent_handlers/_list_stories";
+import RestartStoryIntentHandler from "./intent_handlers/_restart_story";
 import SessionEndedRequestHandler from "./request_handlers/_session_ended";
-import config from "config";
-import * as Stories from "src/routes/story/_stories";
-import * as History from "src/components/Adventure/history";
+import StartedInProgressChooseStoryIntentHandler from "./intent_handlers/_started_in_progress_choose_story";
+import StoryTitleChoiceGivenChooseStoryIntentHandler from "./intent_handlers/_story_title_choice_given_choose_story";
 import {
   SkillRequestSignatureVerifier,
   TimestampVerifier
 } from "ask-sdk-express-adapter";
 import { logger } from "src/logging";
-import { map, join, find } from "lodash";
-
-const asSpeakable = string => {
-  return Alexa.escapeXmlCharacters(string.toLowerCase());
-};
-
-const asConfirmable = string => {
-  return string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
-};
-
-const speechPauseParagraph = () => {
-  return ' <break strength="x-strong"/>';
-};
-
-const speechPauseList = () => {
-  return ' <break strength="medium"/>';
-};
-
-const StartedInProgressChooseStoryIntentHandler = {
-  canHandle(handlerInput) {
-    return (
-      handlerInput.requestEnvelope.request.type === "IntentRequest" &&
-      handlerInput.requestEnvelope.request.intent.name === "ChooseStory"
-    );
-  },
-  handle(handlerInput) {
-    return Stories.list().then(results => {
-      // TODO(kyle): filter by subscriber status (session attribute?)
-      const storyTitleChoices = map(results.rows, story => {
-        return {
-          id: `${story.id}`,
-          name: {
-            value: asConfirmable(asSpeakable(story.title)),
-            synonyms: [
-              asConfirmable(asSpeakable(`${story.title} by ${story.author}`))
-            ]
-          }
-        };
-      });
-
-      const updateStoryTitlesDirective = {
-        type: "Dialog.UpdateDynamicEntities",
-        updateBehavior: "REPLACE",
-        types: [
-          {
-            name: "STORY_TITLE_CHOICE",
-            values: storyTitleChoices
-          }
-        ]
-      };
-
-      const storyList = map(
-        results.rows,
-        story => `${story.title} by ${story.author}`
-      );
-
-      const repeat = "Which tale is next?";
-      const speechText =
-        "Choose a story by saying start followed by the title. You can choose " +
-        join(storyList, speechPauseList());
-
-      return handlerInput.responseBuilder
-        .speak(speechText)
-        .reprompt(repeat)
-        .withSimpleCard("Story choices", join(storyList, ", "))
-        .addDirective(updateStoryTitlesDirective)
-        .withShouldEndSession(false)
-        .getResponse();
-    });
-  }
-};
-
-const storyDecisionChoices = decisions => {
-  return map(decisions, decision => {
-    return {
-      id: decision["storyNode"],
-      name: {
-        value: asConfirmable(asSpeakable(decision["label"])),
-        synonyms: []
-      }
-    };
-  });
-};
-
-const updateStoryDecisionChoicesDirective = choices => {
-  return {
-    type: "Dialog.UpdateDynamicEntities",
-    updateBehavior: "REPLACE",
-    types: [
-      {
-        name: "STORY_DECISION_CHOICE",
-        values: choices
-      }
-    ]
-  };
-};
-
-const asSpeakableStoryText = (story, storyNode, decisionPrompt) => {
-  const justText = join(story["content"][storyNode]["text"], " ");
-  return asSpeakable(justText) + speechPauseList() + decisionPrompt;
-};
-
-const asSpeakableDecisions = decisions => {
-  return (
-    "Your choices are " +
-    speechPauseParagraph() +
-    join(
-      map(decisions, decision => asSpeakable(decision["label"])),
-      " or " + speechPauseList()
-    )
-  );
-};
-
-const findConfirmedSlotValue = (requestEnvelope, key) => {
-  return find(
-    Alexa.getSlot(requestEnvelope, key)["resolutions"][
-      "resolutionsPerAuthority"
-    ],
-    resolution => {
-      return resolution["status"]["code"] === "ER_SUCCESS_MATCH";
-    }
-  )["values"][0]["value"]["id"];
-};
-
-const startFreshStory = (storyId, handlerInput) => {
-  const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
-  sessionAttributes.storyId = storyId;
-  const storyNode = "start";
-  const store = {
-    storyNode: storyNode,
-    history: [
-      {
-        consequences: [],
-        requires: [],
-        storyNode: storyNode
-      }
-    ]
-  };
-  sessionAttributes.store = store;
-  handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
-
-  return Stories.select(storyId).then(results => {
-    const story = results.rows[0];
-    const decisions = History.filterAvailable(
-      story["content"][storyNode]["decisions"],
-      store
-    );
-
-    if (decisions) {
-      const decisionPrompt = asSpeakableDecisions(decisions);
-    } else {
-      const decisionPrompt =
-        "The End. To start another story say " +
-        speechPauseList() +
-        "list stories.";
-    }
-
-    return handlerInput.responseBuilder
-      .speak(
-        "Starting " +
-          story.title +
-          speechPauseParagraph() +
-          asSpeakableStoryText(story, storyNode, decisionPrompt)
-      )
-      .reprompt(decisionPrompt)
-      .withSimpleCard(decisionPrompt)
-      .addDirective(
-        updateStoryDecisionChoicesDirective(storyDecisionChoices(decisions))
-      )
-      .withShouldEndSession(false)
-      .getResponse();
-  });
-};
-
-const StoryTitleChoiceGivenChooseStoryIntentHandler = {
-  canHandle(handlerInput) {
-    return (
-      handlerInput.requestEnvelope.request.type === "IntentRequest" &&
-      handlerInput.requestEnvelope.request.intent.name === "ChooseStory" &&
-      handlerInput.requestEnvelope.request.intent.slots.STORY_TITLE_CHOICE.value
-    );
-  },
-  handle(handlerInput) {
-    const storyId = findConfirmedSlotValue(
-      handlerInput.requestEnvelope,
-      "STORY_TITLE_CHOICE"
-    );
-
-    return startFreshStory(storyId, handlerInput);
-  }
-};
-
-const DecisionGivenChooseStoryDecisionIntentHandler = {
-  canHandle(handlerInput) {
-    return (
-      // TODO(kyle): Are there cases missing here?
-      handlerInput.requestEnvelope.request.type === "IntentRequest" &&
-      handlerInput.requestEnvelope.request.intent.name ===
-        "ChooseStoryDecision" &&
-      handlerInput.requestEnvelope.request.intent.slots.STORY_DECISION_CHOICE
-        .value
-    );
-  },
-  handle(handlerInput) {
-    const decision = findConfirmedSlotValue(
-      handlerInput.requestEnvelope,
-      "STORY_DECISION_CHOICE"
-    );
-    const storyNode = decision;
-
-    const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
-    const storyId = sessionAttributes.storyId;
-    sessionAttributes.store = History.goToDecision(
-      decision,
-      sessionAttributes.store
-    );
-    handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
-
-    return Stories.select(storyId).then(results => {
-      const story = results.rows[0];
-      const decisions = History.filterAvailable(
-        story["content"][storyNode]["decisions"],
-        sessionAttributes.store
-      );
-
-      const decisionPrompt = asSpeakableDecisions(decisions);
-
-      return handlerInput.responseBuilder
-        .speak(asSpeakableStoryText(story, storyNode, decisionPrompt))
-        .reprompt(decisionPrompt)
-        .withSimpleCard(decisionPrompt)
-        .addDirective(
-          updateStoryDecisionChoicesDirective(storyDecisionChoices(decisions))
-        )
-        .withShouldEndSession(false)
-        .getResponse();
-    });
-  }
-};
-
-// TODO(kyle): Add new intents for the other navigation options
-const GoBackIntentHandler = {
-  canHandle(handlerInput) {
-    return (
-      // TODO(kyle): Are there cases missing here?
-      handlerInput.requestEnvelope.request.type === "IntentRequest" &&
-      handlerInput.requestEnvelope.request.intent.name === "GoBack"
-    );
-  },
-  handle(handlerInput) {
-    const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
-    const storyId = sessionAttributes.storyId;
-    sessionAttributes.store = History.goBack(decision, sessionAttributes.store);
-    handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
-
-    const storyNode = sessionAttributes.store.storyNode;
-
-    return Stories.select(storyId).then(results => {
-      const story = results.rows[0];
-      const decisions = History.filterAvailable(
-        story["content"][storyNode]["decisions"],
-        sessionAttributes.store
-      );
-
-      const decisionPrompt = asSpeakableDecisions(decisions);
-
-      return handlerInput.responseBuilder
-        .speak(asSpeakableStoryText(story, storyNode, decisionPrompt))
-        .reprompt(decisionPrompt)
-        .withSimpleCard(decisionPrompt)
-        .addDirective(
-          updateStoryDecisionChoicesDirective(storyDecisionChoices(decisions))
-        )
-        .withShouldEndSession(false)
-        .getResponse();
-    });
-  }
-};
-
-const RestartStoryIntentHandler = {
-  canHandle(handlerInput) {
-    return (
-      // TODO(kyle): Are there cases missing here?
-      handlerInput.requestEnvelope.request.type === "IntentRequest" &&
-      handlerInput.requestEnvelope.request.intent.name === "RestartStory"
-    );
-  },
-  handle(handlerInput) {
-    const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
-    const storyId = sessionAttributes.storyId;
-
-    return startFreshStory(storyId, handlerInput);
-  }
-};
 
 // BEWARE: Order matters; they're handlers.
 const skill = Alexa.SkillBuilders.custom()
@@ -315,6 +23,8 @@ const skill = Alexa.SkillBuilders.custom()
     StoryTitleChoiceGivenChooseStoryIntentHandler,
     StartedInProgressChooseStoryIntentHandler,
     DecisionGivenChooseStoryDecisionIntentHandler,
+    GoBackIntentHandler,
+    RestartStoryIntentHandler,
     HelpIntentHandler,
     CancelAndStopIntentHandler,
     SessionEndedRequestHandler
@@ -322,7 +32,7 @@ const skill = Alexa.SkillBuilders.custom()
   .addErrorHandlers(ErrorHandler)
   .create();
 
-const post = (req, res, next) => {
+export const post = (req, res, next) => {
   logger.info(req.body, "Alexa request received");
   return new SkillRequestSignatureVerifier()
     .verify(req.rawBody, req.headers)
@@ -340,5 +50,3 @@ const post = (req, res, next) => {
       res.status(500);
     });
 };
-
-export { post };
