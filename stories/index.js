e89ebd4ebb3264d0acc1fb6e59ea1d2d4d7fd4d2
Kristopher Paulsen <kristopherpaulsen@gmail.com>
2020-02-29 06:31:19 -0700

    add the apprentice

--- stories/index.js
+++ stories/index.js
@@ -2,10 +2,12 @@ const mr_banks = require("./mr_banks.js");
 const wagons_and_dragons = require("./wagons_and_dragons.js");
 const forgotten_forest = require("./forgotten_forest.js");
 const playing_detective = require("./playing_detective.js");
+const the_apprentice_and_the_dragon = require("./the_apprentice_and_the_dragon");
 
 module.exports = [
   mr_banks,
   wagons_and_dragons,
   forgotten_forest,
-  playing_detective
+  playing_detective,
+  the_apprentice_and_the_dragon,
 ];
