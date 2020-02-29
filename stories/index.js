85a0d11acae49720fbcac2f215675e0d0d4c0850
Kristopher Paulsen <kristopherpaulsen@gmail.com>
2020-02-29 06:38:10 -0700

    orc orc orcs!

--- stories/index.js
+++ stories/index.js
@@ -3,6 +3,7 @@ const wagons_and_dragons = require("./wagons_and_dragons.js");
 const forgotten_forest = require("./forgotten_forest.js");
 const playing_detective = require("./playing_detective.js");
 const the_apprentice_and_the_dragon = require("./the_apprentice_and_the_dragon");
+const heroes_and_monsters = require("./heroes_and_monsters.js");
 
 module.exports = [
   mr_banks,
@@ -10,4 +11,5 @@ module.exports = [
   forgotten_forest,
   playing_detective,
   the_apprentice_and_the_dragon,
+  heroes_and_monsters,
 ];
