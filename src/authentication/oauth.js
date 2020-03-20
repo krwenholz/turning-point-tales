15a91671bdd4918e7dbda8db5fda06e8f32b4bb5
Kyle R Wenholz <kyle@krwenholz.com>
2020-03-19 19:55:48 -0700

    Simplify, maybe, our oauth routes

--- src/authentication/oauth.js
+++ src/authentication/oauth.js
@@ -196,3 +196,49 @@ export const tokenStrategy = new passportHttpBearer.Strategy(
     });
   }
 );
+
+const findAccessTokenByUserIdAndClientId = async (userId, clientId, done) => {
+  try {
+    const results = await pool.query(
+      `
+    SELECT
+        client_id AS clientId,
+        user_id AS userId
+    FROM
+        oauth_access_tokens
+    WHERE
+        user_id = $1
+        AND client_id = $2
+    `,
+      [userId, clientId]
+    );
+
+    return done(null, results.rows[0]);
+  } catch (err) {
+    logger.error(err);
+    return done(err);
+  }
+};
+
+export const authorize = [
+  server.authorize(
+    (clientId, redirectUri, done) => {
+      if (
+        !includes(config.get("alexa.redirectUris"), redirectUri) &&
+        clientId !== conifg.get("alexa.clientId")
+      ) {
+        return done(null);
+      }
+      return done(null, { clientId, isTrusted: true }, redirectUri);
+    },
+    (client, user, done) => {
+      // We don't do immediate approval, so always prompt user for it
+      return done(null, false);
+    }
+  ),
+  (req, res) => {
+    return res.redirect(
+      `/oauth/authorize/dialog?transactionId=${req.oauth2.transactionID}`
+    );
+  }
+];
--- src/routes/oauth/authorize/decision.js
+++
@@ -1,8 +0,0 @@
-import { logger } from "src/logging";
-import { server } from "src/authentication/oauth";
-
-const post = (req, res, next) => {
-  return server.decision()(req, res, next);
-};
-
-export { post };
