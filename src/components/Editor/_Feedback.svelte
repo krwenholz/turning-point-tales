a50262d619810f38ff20f0994f0060457fe8537c
Kristopher Paulsen <kristopherpaulsen@gmail.com>
2020-03-21 09:51:06 -0600

    add simple error feedback

---
+++ src/components/Editor/_Feedback.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let errors = [];
+</script>
+
+<style>
+  .feedback {
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+    flex: 1;
+    padding-top: 24px;
+  }
+</style>
+
+
+<div class="feedback">
+  {#if errors.length}
+    <b>Feedback</b>
+  {/if}
+
+  <ul>
+    {#each errors as error}
+    <li class='error'>- {error.message}</li>
+    {/each}
+  </ul>
+</div>
