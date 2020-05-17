7fb256f986f899ea0c76350f42060df6c118033f
Kyle R Wenholz <kyle@krwenholz.com>
2020-05-17 08:50:26 -0700

    Enable immediate story start on Alexa launch

--- src/alexa/request_handlers/_launch.js
+++ src/alexa/request_handlers/_launch.js
@@ -1,4 +1,7 @@
+import * as Stories from "src/routes/story/_stories";
+import { asConfirmable, asSpeakable } from "src/alexa/_utilities";
 import { logger } from "src/logging";
+import { map, join, filter, find, lastIndexOf } from "lodash";
 
 const LaunchRequestHandler = {
   canHandle(handlerInput) {
@@ -7,12 +10,47 @@ const LaunchRequestHandler = {
   handle(handlerInput) {
     const speechText =
       "Welcome to Turning Point Tales. You can list tales or choose your next adventure.";
+    const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
 
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .reprompt(speechText)
-      .withSimpleCard("Turning Point Tales", speechText)
-      .getResponse();
+    return Stories.list().then(results => {
+      const stories = filter(
+        results.rows,
+        story => sessionAttributes.user.isSubscribed || story.general_release
+      );
+
+      const storyTitleChoices = map(stories, story => {
+        return {
+          id: `${story.id}`,
+          name: {
+            value: asConfirmable(asSpeakable(story.title)),
+            synonyms: [
+              asConfirmable(asSpeakable(`${story.title} by ${story.author}`)),
+              asConfirmable(
+                asSpeakable(story.title.slice(lastIndexOf(story.title, "-")))
+              )
+            ]
+          }
+        };
+      });
+
+      const updateStoryTitlesDirective = {
+        type: "Dialog.UpdateDynamicEntities",
+        updateBehavior: "REPLACE",
+        types: [
+          {
+            name: "STORY_TITLE_CHOICE",
+            values: storyTitleChoices
+          }
+        ]
+      };
+
+      return handlerInput.responseBuilder
+        .speak(speechText)
+        .reprompt(speechText)
+        .withSimpleCard("Turning Point Tales", speechText)
+        .addDirective(updateStoryTitlesDirective)
+        .getResponse();
+    });
   }
 };
 
