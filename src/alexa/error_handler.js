91d74412308ada6a9c083c8b3ab1d4005be8d5da
Kyle R Wenholz <kyle@krwenholz.com>
2020-06-20 09:10:22 -0700

    Add slightly more helpful Alexa text

--- src/alexa/error_handler.js
+++ src/alexa/error_handler.js
@@ -6,11 +6,12 @@ const ErrorHandler = {
     return true;
   },
   handle(handlerInput, error) {
-    // TODO(kyle): This is stupid and needs help
     logger.info(error, "Alexa error handled");
 
     return handlerInput.responseBuilder
-      .speak("Sorry, I can't understand the command. Please say again.")
+      .speak(
+        "Sorry, I can't understand the command. Please say again or contact support at h2wib dot com if your problem continues."
+      )
       .reprompt("Sorry, I can't understand the command. Please say again.")
       .getResponse();
   }
